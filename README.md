61d448aea84eb2f754d2720302cfd907830d6406
Wouter van den Brink <git@woutervdb.com>
2023-01-13 15:32:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Buienradar
+
+JavaScript-library om eenvoudig data uit de Buienradar API op te halen en weer te geven. Deze library wordt op verschillende plekken gebruikt in onze online informaticamethode [Fundament Informatica](https://www.instruct.nl/methoden/fundament-informatica/). Zo kun je in domein A aan de slag om zelf een app te maken die de data van Buienradar gebruikt om weerinformatie aan de gebruiker te laten zien. En op een aantal andere plekken maken we gebruik van de library om real-time weerinformatie te gebruiken in de opdrachten en tekst.
+
+## Installatie en gebruik
+TODO! Voor nu kun je [de demopagina](examples/demo.html) bekijken om te zien hoe de library gebruikt moet worden.
+
+## Contact
+Heb je vragen over het gebruik van deze library, of over onze informaticamethode? Mail dan gerust naar [fundament@instruct.nl](mailto:fundament@instruct.nl).
---
+++ src/index.js
@@ -0,0 +1,382 @@
+(function () {
+    const BUIENRADAR_API = 'https://data.buienradar.nl/2.0/feed/json';
+
+    const CACHE_MS = 300_000;
+
+    const LS_UPDATED_AT = 'nl.instruct.buienradar.updatedAt';
+    const LS_CACHE = 'nl.instruct.buienradar.cache';
+
+    const ERROR_IMG = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAAUUlEQVQoz2P4DwQMDP+JAWBlDCAKggiqBiMGZA5B1WAbMITwqEbSgEsPhiBeaWxGMOB0AA5H4nU0Ni9RroE0J5HmadKClbSIIy1pkJn4iE7eALWZVcf6S1ptAAAAAElFTkSuQmCC';
+
+    const POS_INSTRUCT = {lat: 52.071, lon: 4.763};
+
+    let data = null;
+    let pos = POS_INSTRUCT;
+
+    async function load(callback, refresh = false) {
+        const updatedAt = parseInt(localStorage.getItem(LS_UPDATED_AT), 10);
+
+        try {
+            data = JSON.parse(localStorage.getItem(LS_CACHE));
+        } catch (e) {
+        }
+
+        const delta = (+new Date()) - updatedAt;
+
+        if (refresh || delta > CACHE_MS || data === null) {
+            const res = await fetch(BUIENRADAR_API);
+            data = await res.json();
+            localStorage.setItem(LS_UPDATED_AT, +new Date());
+        }
+
+        localStorage.setItem(LS_CACHE, JSON.stringify(data));
+
+        pos = await getLocation();
+
+        callback.bind(window)();
+    }
+
+    async function getLocation() {
+        if (!navigator.geolocation) {
+            // Instruct B.V. :-)
+            return POS_INSTRUCT;
+        }
+
+        try {
+            const pos = await new Promise((resolve, reject) => {
+                navigator.geolocation.getCurrentPosition(resolve, reject);
+            });
+            return {lat: pos.coords.latitude, lon: pos.coords.longitude};
+        } catch (e) {
+            return POS_INSTRUCT;
+        }
+    }
+
+    function deg2rad(deg) {
+        return deg * (Math.PI / 180);
+    }
+
+    function getDistance(pos1, pos2) {
+        const R = 6371;
+
+        const dLat = deg2rad(pos2.lat - pos1.lat);
+        const dLon = deg2rad(pos2.lon - pos1.lon);
+
+        const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(pos1.lat)) * Math.cos(deg2rad(pos2.lat)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return R * c;
+    }
+
+    function getNearestWeatherStation(search) {
+        return data.actual.stationmeasurements.reduce(({nearest, distance}, station) => {
+            const stationPos = {lat: station.lat, lon: station.lon};
+            const stationDistance = getDistance(pos, stationPos);
+            if (stationDistance < distance && station[search]) {
+                return {nearest: station, distance: stationDistance};
+            } else {
+                return {nearest, distance};
+            }
+        }, {nearest: null, distance: Infinity}).nearest ?? null;
+    }
+
+    function renderer(resolver, attribute = 'textContent', errorValue = '-') {
+        return async (id) => {
+            const e = document.getElementById(id);
+
+            if (e === null) {
+                console.error(`Je probeerde informatie weer te geven op een niet-bestaand element met ID ${id}!`);
+                return;
+            }
+
+            try {
+                e[attribute] = await resolver();
+            } catch (e) {
+                console.error(e);
+                e[attribute] = errorValue;
+            }
+        };
+    }
+
+    function rendererForNearestStation(property, attribute = 'textContent', errorValue = '-') {
+        return renderer(() => {
+            const station = getNearestWeatherStation(property);
+
+            if (station && station[property]) {
+                return station[property];
+            }
+
+            throw new Error(`Geen station gevonden met eigenschap ${property}`);
+        }, attribute, errorValue);
+    }
+
+    function rendererForStation(property, attribute = 'textContent', errorValue = '-') {
+        return (stationId, id) => {
+            renderer(() => {
+                const station = data.actual.stationmeasurements.find((s) => s.stationid === stationId);
+
+                if (!station) {
+                    throw new Error(`Station met ID ${stationId} bestaat niet`);
+                }
+
+                if (typeof station[property] !== 'undefined') {
+                    return station[property];
+                }
+
+                throw new Error(`Station ${stationId} heeft geen eigenschap ${property}`);
+            }, attribute, errorValue)(id);
+        };
+    }
+
+    function rendererForNestedProperty(nestedProperty, attribute = 'textContent', errorValue = '-') {
+        return renderer(nestedPropertyResolver(nestedProperty), attribute, errorValue);
+    }
+
+    function nestedPropertyResolver(nestedProperty) {
+        return () => {
+            const parts = nestedProperty.split('.');
+            let cursor = data;
+
+            for (let part of parts) {
+                if (cursor[part]) {
+                    cursor = cursor[part];
+                } else {
+                    throw new Error(`Eigenschap ${part} niet gevonden in cursor (query: ${nestedProperty})`);
+                }
+            }
+
+            return cursor;
+        };
+    }
+
+    function rendererForFiveDayForecast(property, attribute = 'textContent', errorValue = '-') {
+        return (id, day) => {
+            renderer(() => fiveDayForecastResolver(property)(day), attribute, errorValue)(id);
+        };
+    }
+
+    function fiveDayForecastResolver(property) {
+        return (day) => {
+            if (day < 1 || day > 5) {
+                throw new Error(`Dag ${day} is niet geldig`);
+            }
+
+            if (data.forecast.fivedayforecast.length !== 5) {
+                throw new Error('Onverwacht aantal voorspellingen in de vijfdaagse voorspelling');
+            }
+
+            const forecast = data.forecast.fivedayforecast[day - 1];
+
+            if (forecast && typeof forecast[property] !== 'undefined') {
+                return forecast[property];
+            }
+
+            throw new Error(`Eigenschap ${property} niet gevonden in voorspelling #${day}`);
+        };
+    }
+
+    function rendererForDate(resolver) {
+        return (id, date) => {
+            if (Object.prototype.toString.call(date) !== '[object Date]') {
+                date = new Date(date);
+            }
+
+            renderer(() => resolver(date))(id);
+        };
+    }
+
+    async function showRainChart(id) {
+        const e = document.getElementById(id);
+
+        if (e === null) {
+            console.error(`Je probeerde informatie weer te geven op een niet-bestaand element met ID ${id}!`);
+            return;
+        }
+
+        const res = await fetch(`https://gpsgadget.buienradar.nl/data/raintext?lat=${pos.lat}&lon=${pos.lon}`);
+        const dataPoints = (await res.text()).split("\n").map((line) => {
+            const matches = line.match(/^(\d{3})\|(\d\d:\d\d)/);
+
+            if (matches === null) {
+                return null;
+            }
+
+            const value = parseInt(matches[1], 10);
+            const time = matches[2];
+
+            const mmPerHour = Math.pow(10, ((value - 109) / 32)).toFixed(1);
+
+            return {time, mmPerHour};
+        }).filter(v => v !== null);
+
+        const canvas = document.createElement('canvas');
+        canvas.style.width = '100%';
+
+        e.appendChild(canvas);
+
+        const config = {
+            type: 'bar',
+            data: {
+                labels: dataPoints.map((p) => p.time),
+                datasets: [
+                    {
+                        label: 'Neerslag (mm/uur)',
+                        data: dataPoints.map((p) => p.mmPerHour),
+                        backgroundColor: '#0064ff'
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        suggestedMax: 1
+                    }
+                }
+            }
+        };
+
+        new Chart(canvas, config);
+    }
+
+    window.buienradar = {
+        load,
+        date: {
+            name: rendererForDate((date) => {
+                return ['zondag', 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag'][date.getDay()];
+            }),
+            date: rendererForDate((date) => {
+                const year = date.getFullYear();
+                const month = date.getMonth() + 1;
+                const day = date.getDate();
+
+                return `${year}-${month < 10 ? '0' : ''}${month}-${day < 10 ? '0' : ''}${day}`;
+            }),
+            dayMonth: rendererForDate((date) => {
+                const month = date.getMonth() + 1;
+                const day = date.getDate();
+
+                return `${day < 10 ? '0' : ''}${day}-${month < 10 ? '0' : ''}${month}`;
+            }),
+            time: rendererForDate((date) => {
+                const hour = date.getHours();
+                const minute = date.getMinutes();
+
+                return `${hour < 10 ? '0' : ''}${hour}:${minute < 10 ? '0' : ''}${minute}`;
+            })
+        },
+        buienradar: {
+            copyright: rendererForNestedProperty('buienradar.copyright'),
+            terms: rendererForNestedProperty('buienradar.terms'),
+        },
+        sunrise: nestedPropertyResolver('actual.sunrise'),
+        sunset: nestedPropertyResolver('actual.sunset'),
+        map: rendererForNestedProperty('actual.actualradarurl', 'src', ERROR_IMG),
+        currentLocation: {
+            cityName: renderer(async () => {
+                const res = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${pos.lat}&lon=${pos.lon}`);
+                const data = await res.json();
+                if (data && data.address) {
+                    return data.address.city ?? data.address.town ?? '-';
+                }
+            }),
+            station: {
+                name: rendererForNearestStation('stationname'),
+                regio: rendererForNearestStation('regio'),
+            },
+            icon: rendererForNearestStation('iconurl', 'src', ERROR_IMG),
+            description: rendererForNearestStation('weatherdescription'),
+            temperature: {
+                actual: rendererForNearestStation('temperature'),
+                ground: rendererForNearestStation('groundtemperature'),
+                feel: rendererForNearestStation('feeltemperature'),
+            },
+            wind: {
+                direction: rendererForNearestStation('windidrection'),
+                directionDegrees: rendererForNearestStation('winddirectiondegrees'),
+                gusts: rendererForNearestStation('windgusts'),
+                speed: rendererForNearestStation('windspeed'),
+                speedBft: rendererForNearestStation('windspeedBft'),
+            },
+            airPressure: rendererForNearestStation('airpressure'),
+            visibiliy: rendererForNearestStation('visibility'),
+            humidity: rendererForNearestStation('humidity'),
+            sunPower: rendererForNearestStation('sunpower'),
+            rain: {
+                now: rendererForNearestStation('precipation'),
+                last24Hours: rendererForNearestStation('rainFallLast24Hour'),
+                lastHour: rendererForNearestStation('rainFallLastHour'),
+            },
+
+        },
+        weatherStation: {
+            station: {
+                name: rendererForStation('stationname'),
+                regio: rendererForStation('regio'),
+            },
+            icon: rendererForStation('iconurl', 'src', ERROR_IMG),
+            description: rendererForStation('weatherdescription'),
+            temperature: {
+                actual: rendererForStation('temperature'),
+                ground: rendererForStation('groundtemperature'),
+                feel: rendererForStation('feeltemperature'),
+            },
+            wind: {
+                direction: rendererForStation('windidrection'),
+                directionDegrees: rendererForStation('winddirectiondegrees'),
+                gusts: rendererForStation('windgusts'),
+                speed: rendererForStation('windspeed'),
+                speedBft: rendererForStation('windspeedBft'),
+            },
+            airPressure: rendererForStation('airpressure'),
+            visibiliy: rendererForStation('visibility'),
+            humidity: rendererForStation('humidity'),
+            sunPower: rendererForStation('sunpower'),
+            rain: {
+                now: rendererForStation('precipation'),
+                last24Hours: rendererForStation('rainFallLast24Hour'),
+                lastHour: rendererForStation('rainFallLastHour'),
+            },
+        },
+        forecast: {
+            report: {
+                published: nestedPropertyResolver('forecast.weatherreport.published'),
+                title: rendererForNestedProperty('forecast.weatherreport.title'),
+                summary: rendererForNestedProperty('forecast.weatherreport.summary'),
+                text: rendererForNestedProperty('forecast.weatherreport.text', 'innerHTML'),
+                author: rendererForNestedProperty('forecast.weatherreport.author'),
+                authorBio: rendererForNestedProperty('forecast.weatherreport.authorbio'),
+            },
+            shortTerm: {
+                start: nestedPropertyResolver('forecast.shortterm.startdate'),
+                end: nestedPropertyResolver('forecast.shortterm.enddate'),
+                forecast: rendererForNestedProperty('forecast.shortterm.forecast')
+            },
+            longTerm: {
+                start: nestedPropertyResolver('forecast.longterm.startdate'),
+                end: nestedPropertyResolver('forecast.longterm.enddate'),
+                forecast: rendererForNestedProperty('forecast.longterm.forecast')
+            },
+            fiveDays: {
+                date: fiveDayForecastResolver('day'),
+                temperature: {
+                    min: rendererForFiveDayForecast('mintemperature'),
+                    max: rendererForFiveDayForecast('maxtemperature'),
+                },
+                rain: {
+                    chance: rendererForFiveDayForecast('rainChance'),
+                    mmMin: rendererForFiveDayForecast('mmRainMin'),
+                    mmMax: rendererForFiveDayForecast('mmRainMax'),
+                },
+                sunChance: rendererForFiveDayForecast('sunChance'),
+                wind: {
+                    direction: rendererForFiveDayForecast('windDirection'),
+                    speed: rendererForFiveDayForecast('wind'),
+                },
+                description: rendererForFiveDayForecast('weatherdescription'),
+                icon: rendererForFiveDayForecast('iconurl', 'src', ERROR_IMG),
+            }
+        },
+        showRainChart
+    };
+})();
